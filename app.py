f97d972bbfcac5e026ff9e6a58c7414452f9ce58
OctavioGalvan5 <octaviogalvan2003@gmail.com>
2024-09-11 04:52:47 +0000

    Se agregaron dos nuevos archivos html, menu ahora es el principal, home ya no se usa

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello_world():
-  return render_template('home.html')
+    return render_template('menu.html')
 
+@app.route('/calculadora_percibido')
+def Calculadora_Percibido():
+    return render_template('prueba.html')
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True)
