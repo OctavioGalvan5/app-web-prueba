e9ab5f36546c214375df74be068e0b8a517a24d8
OctavioGalvan5 <octaviogalvan2003@gmail.com>
2024-11-20 23:03:21 +0000

    se esta solucionando el problema de kaleido

--- models/entities/User.py
+++ models/entities/User.py
@@ -21,3 +21,4 @@ class User(UserMixin):
 #password= "Belgrano1188"
 #hashed_password = generate_password_hash(password, method='pbkdf2:sha256', salt_length=16)
 #print(hashed_password)
+# python models/entities/User.py
